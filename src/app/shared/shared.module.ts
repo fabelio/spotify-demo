c9f4516a62b4f9b08b33165fef95f2e585ac2563
fajtun <fajtun@beesion.com>
2024-10-30 08:50:54 -0600

    init commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderUserComponent,
+    MediaPlayerComponent,
+    SideBarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
